34e4396f78e3d35a7790a45d3b4fad18f6dd07f5
“jundalisay” <jundalisay@yahoo.com>
2025-04-15 15:09:47 +0800

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./layouts/**/*.{html,js}", "./content/**/*.{md,html}", "./hugo_stats.json", "./assets/**/*.{js,css}"],
+  content: [
+    "./layouts/**/*.{html,js}",
+    "./themes/**/layouts/**/*.{html,js}",
+    "./content/**/*.{md,html}",
+    "./hugo_stats.json", 
+    "./assets/**/*.{js,css}"
+  ],
   theme: {
     extend: {
       fontFamily: {
@@ -9,7 +15,6 @@ module.exports = {
     },
   },
   plugins: [
-    require('@tailwindcss/typography'),
+    require('@tailwindcss/typography'), require('@tailwindcss/aspect-ratio'),
   ],
 }
-
